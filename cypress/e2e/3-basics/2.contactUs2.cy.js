1216d7efd47b1927f21bda55f5c894b4f945e7b3
DipanshuChawde <dipanshuchoude@gmail.com>
2025-04-21 20:24:46 +0530

    21Apr25

---
+++ cypress/e2e/3-basics/2.contactUs2.cy.js
@@ -0,0 +1,27 @@
+///<reference types = "cypress" />
+
+describe("verify contact us page for valid and invalid data", () => {
+    it("verify contact us page for valid data", () => {
+        //AAA
+        cy.visit('https://www.webdriveruniversity.com/Contact-Us/contactus.html')
+
+        cy.get('[name="first_name"]').type('dipanshu')
+        cy.get('[name="last_name"]').type('chawde')
+        cy.get('[name="email"]').type('dipanshu@gmail.com')
+        cy.get('[name="message"]').type('hi , how are you')
+        cy.get('[type="submit"]').click()
+        cy.get('h1').should('have.text', 'Thank You for your Message!')
+
+    })
+    it("verify contact us page for in-valid data", () => {
+        cy.visit('https://www.webdriveruniversity.com/Contact-Us/contactus.html')
+
+        cy.get('[name="first_name"]').type('dipanshu')
+        cy.get('[name="last_name"]').type('chawde')
+        cy.get('[name="email"]').type('dipanshu')
+        cy.get('[name="message"]').type('hi , how are you')
+        cy.get('[type="submit"]').click()
+        cy.contains('Error: Invalid email address').should('be.visible')
+    })
+
+})
