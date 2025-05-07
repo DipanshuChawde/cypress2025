653d17532d21b9b1822d05896af472e8f89f6328
DipanshuChawde <dipanshuchoude@gmail.com>
2025-05-07 20:35:50 +0530

    7May25

--- cypress/e2e/3-basics/10.dropdown2.cy.js
+++ cypress/e2e/3-basics/10.dropdown2.cy.js
@@ -2,11 +2,20 @@
 
 describe('verify dropdown in cypress', () => {
 
-    it('static drop down', () => {
+    it('prime dropdown', () => {
         cy.visit('https://www.primevideo.com/offers/nonprimehomepage/ref=atv_dl_rdr')
         cy.get('[aria-label="Search Prime Video"]').eq(0).click()
 
-        cy.get('input#pv-search-nav').type('chor')
-        
+        cy.get('input#pv-search-nav').eq(0).type('chor')
+
+        cy.get('ol > li > a').each((el, idx)=>{
+            //cy.log(el.text())
+            if(el.text()=='chor machaye shor'){
+                cy.wrap(el).click()
+            }
+        })
+        cy.contains('Results for "chor machaye shor".').should('be.visible')
+       
     })
 })
+
