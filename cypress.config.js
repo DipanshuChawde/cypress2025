80fc432964b008ab3dc36e067b52a1138bdbcba1
DipanshuChawde <dipanshuchoude@gmail.com>
2025-05-27 20:39:01 +0530

    27May25

--- cypress.config.js
+++ cypress.config.js
@@ -3,6 +3,7 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     //chromeWebSecurity: false,
+    //includeShadowDom : true ,
     setupNodeEvents(on, config) {
       // implement node event listeners here
       //14.cyTask.cy.js------------------------
