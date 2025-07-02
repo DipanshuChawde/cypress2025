10803493a3cef2cd4669082a4957fbfcffb8c9b6
DipanshuChawde <dipanshuchoude@gmail.com>
2025-07-02 21:08:46 +0530

    02July25

---
+++ cypress/fixtures/gorestPost.js
@@ -0,0 +1,18 @@
+export default
+    [
+        { "name": "Dipanshu", 
+            "gender": "female", 
+            "email": `dipanshu${Date.now()}@gmail.com`, 
+            "status": "active" 
+        },
+        { "name": "Rucha", 
+            "gender": "female", 
+            "email": `rucha${Date.now()}@gmail.com`, 
+            "status": "active" 
+        },
+        { "name": "Aditya", 
+            "gender": "male", 
+            "email": `aditya${Date.now()}@gmail.com`, 
+            "status": "active" 
+        },
+    ]
---
+++ cypress/fixtures/gorestPut.js
@@ -0,0 +1,18 @@
+export default
+    [
+        {
+            "name": "Dipanshu Chawde",
+            "email": `dipanshuC${Date.now()}@gmail.com`,
+            "status": "active"
+        },
+        {
+            "name": "Rucha Gaware",
+            "email": `ruchaG${Date.now()}@gmail.com`,
+            "status": "active"
+        },
+        {
+            "name": "Aditya Masalkar",
+            "email": `adityaM${Date.now()}@gmail.com`,
+            "status": "active"
+        },
+    ]
